42d5d10eb3a4086eafe092a5d8ac2a6b623609b6
Sar2580P <sporwal1818@gmail.com>
2023-10-10 11:19:57 +0530

    "Created lightening train_eval setup"

--- Preprocessing/train_eval.py
+++ Preprocessing/train_eval.py
@@ -1,131 +1,72 @@
 import torch
+import pandas as pd
 import pytorch_lightning as pl
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
-from tqdm import tqdm
-import pickle
-import os
 from PIL import Image
+import torchmetrics
 
-class Classifier(torch.nn.Module):
+class Classifier(pl.LightningModule):
+  def __init__(self, model_obj, train_df:pd.DataFrame, val_df:pd.DataFrame, tst_df:pd.DataFrame):
+    super().__init__()
+    self.model = model_obj.model
+    self.config = model_obj.config
+    self.transforms_ = model_obj.transforms_
+    self.train_df = train_df
+    self.val_df = val_df
+    self.tst_df = tst_df
 
-  def __init__(self, model_obj  ,
-                optimizer, 
-               df_tr, df_val , lr_scheduler = None):
-    
-    super(Classifier, self).__init__()
-    print('Classifier object created')
-    self.df_tr = df_tr
-    self.df_val = df_val
-    self.model_obj = model_obj
-    self.optimizer = optimizer
+    self.accuracy = torchmetrics.Accuracy()
     self.criterion = torch.nn.CrossEntropyLoss()
-    self.lr_scheduler = lr_scheduler
-    self.valid_loss_min = np.Inf
-    self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-  def forward(self, x):
-      return self.model_obj.forward(x)
+  def setup(self, stage: str):
+    if stage=='fit':
+      self.train_dataset = MyDataset(self.train_df, self.transforms_)
+      self.val_dataset = MyDataset(self.val_df, self.transforms_)
+    elif stage == 'test':
+      self.tst_dataset = MyDataset(self.tst_df, self.transforms_)
 
-  def loss_function(self, logits, labels):
-    return self.loss(logits, labels)
-
-  def __training_step__(self)->dict:
-      self.model_obj.model.train()
-      epoch_acc, epoch_loss = 0, 0
-      pbar = tqdm(self.tr_loader)
-
-      for batch_idx, (inputs, targets) in enumerate(pbar):
-          # inputs, targets = inputs.to(self.device), targets.to(self.device)
-          outputs = self.forward(inputs)
-          loss = self.criterion(outputs, targets.long())
-          self.optimizer.zero_grad()    # clear the gradients of all optimized variables
-          loss.backward()               # back_prop: compute gradient of the loss with respect to model parameters
-          self.optimizer.step()         # parameter update
-          epoch_loss += loss.item()
-          _, pred = torch.max(outputs, dim=1)
-          correct = torch.sum(pred==targets).item()
-          total = targets.size(0)
-          epoch_acc += correct/total
-
-      tr_loss = epoch_loss/self.total_steps_tr
-      tr_acc = epoch_acc/self.total_steps_tr
-      return {'loss':tr_loss, 'acc':tr_acc}
+  def train_dataloader(self):
+    return DataLoader(self.train_dataset, batch_size= self.config['BATCH_SIZE'], shuffle=True, num_workers=4)
   
-
-  def __validation_step__(self)->dict:
-      self.model_obj.model.eval()
-      epoch_loss, epoch_acc = 0 , 0
-      pbar = tqdm(self.val_loader)
-
-      with torch.no_grad():           # disable gradient calculation
-        for batch_idx, (inputs, targets) in enumerate(pbar):
-          # inputs, targets = inputs.to(self.device), targets.to(self.device)
-          outputs = self.forward(inputs)
-          loss = self.criterion(outputs, targets.long())
-
-          epoch_loss += loss.item()
-          _, pred = torch.max(outputs, dim=1)
-          correct = torch.sum(pred==targets).item()
-          total = targets.size(0)
-          epoch_acc += correct/total
-
-        val_loss = epoch_loss/self.total_steps_val
-        val_acc = epoch_acc/self.total_steps_val
-      return {'loss':val_loss, 'acc':val_acc}
+  def val_dataloader(self):
+    return DataLoader(self.val_dataset, batch_size=self.config['BATCH_SIZE'], shuffle=True, num_workers=4 )
   
-  def __prepare_data__(self):
-    # transforms for images
-    tr_dataset = MyDataset(self.df_tr , self.model_obj.transforms_)
-    self.tr_loader = DataLoader(tr_dataset, batch_size=self.model_obj.config['BATCH_SIZE'], shuffle=True)
-    val_dataset = MyDataset(self.df_val , self.model_obj.transforms_)
-    self.val_loader = DataLoader(val_dataset, batch_size=self.model_obj.config['BATCH_SIZE'], shuffle=True)
-    self.total_steps_tr = len(self.tr_loader)
-    self.total_steps_val = len(self.val_loader)
+  def test_dataloader(self):
+    return DataLoader(self.tst_dataset, self.config['BATCH_SIZE'], shuffle=False, num_workers=4)
   
-  def run(self, epochs):
-    self.__prepare_data__()
+  def training_step(self, batch, batch_idx):
+    x, y = batch
+    y_hat = self.model(x)
+    loss = self.criterion(y_hat, y)
+    self.accuracy(y_hat, y)
+    self.log("train_acc", self.accuracy, on_epoch=True,prog_bar=True, logger=True)
+    self.log("train_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+    return loss
+  
+  def validation_step(self, batch, batch_idx):
+    x, y = batch
+    y_hat = self.model(x)
+    loss = self.criterion(y_hat, y)
+    self.accuracy(y_hat, y)
+    self.log("val_acc", self.accuracy, on_epoch=True,prog_bar=True, logger=True)
+    self.log("val_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+    return loss
+  
+  def test_step(self, batch, batch_idx):
+    x, y = batch
+    y_hat = self.model(x)
+    loss = self.criterion(y_hat, y)
+    self.accuracy(y_hat, y)
+    self.log("test_acc", self.accuracy, on_epoch=True,prog_bar=True, logger=True)
+    self.log("test_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+    return loss
+  
+  def configure_optimizers(self):
+    optim =  torch.optim.Adam(self.model.parameters(), lr=self.config['lr'])
+    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, patience=5, threshold=0.001, cooldown =2,verbose=True)
+    return [optim], [{'scheduler': lr_scheduler, 'interval': 'epoch', 'monitor': 'train_loss', 'name': 'lr_scheduler'}]
 
-    train_losses, train_acc = [] , []
-    test_losses, test_acc  = [] , []
-    
-    for epoch in range(epochs):
-      print(f'\nEpoch: {epoch}')
-      train_logs = self.__training_step__()
-      val_logs = self.__validation_step__()
-      
-      
-      
-      print ('Epoch [{}] --> LossTr: {:.4f}    AccTr: {:.4f}'
-              .format(epoch, train_logs['loss'],train_logs['acc']), end = '    ')
-      print('lossVal : {:.4f}     accVal : {:.4f}\n'.format(val_logs['loss'] , val_logs['acc']))
-
-      train_losses.append(train_logs['loss'])
-      train_acc.append(train_logs['acc'])
-      test_losses.append(val_logs['loss'])
-      test_acc.append(val_logs['acc'])
-
-      if self.lr_scheduler is not None:
-        self.lr_scheduler.step(train_losses[-1])   # updating learning rate
-
-      network_learned = self.valid_loss_min - test_losses[-1] > 0.01
-      if network_learned:
-        self.valid_loss_min = test_losses[-1]
-        torch.save(self.model_obj.model.state_dict(), os.path.join(self.model_obj.config['dir'] , 'model_{name}.pt'.format(name = self.model_obj.config['name'])))
-        print('Detected network improvement, saving current model  "\u2705"')
-
-    history = {
-      'train_loss': train_losses,
-      'train_acc': train_acc,
-      'val_loss': test_losses,
-      'val_acc': test_acc
-    }
-
-    with open(os.path.join(self.model_obj.config['dir'] ,'history.pickle'), 'wb') as handle:
-      pickle.dump(history, handle, protocol=pickle.HIGHEST_PROTOCOL)
-      handle.close()
-
-    return history
 #___________________________________________________________________________________________________________________
 class MyDataset(Dataset):
   # defining values in the constructor
