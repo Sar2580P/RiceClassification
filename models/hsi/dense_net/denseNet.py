d30c8c371a734580eb911aa3cf30bea258cf1986
Sar2580P <sporwal1818@gmail.com>
2023-12-18 09:32:53 +0530

    d

--- models/hsi/dense_net/denseNet.py
+++ models/hsi/dense_net/denseNet.py
@@ -8,7 +8,7 @@ from callbacks import *
 from utils import *
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger, CSVLogger
-from data_loading import *
+# from data_loading import *
 
 config_path = 'models/hsi/dense_net/config.yaml'
 config = load_config(config_path)
@@ -26,26 +26,26 @@ model_obj = DenseNet(densenet_variant = model_parameters['densenet121'] , in_cha
 # model = Classifier.load_from_checkpoint(hsi_ckpt, model_obj=model_obj)
 
 #___________________________________________________________________________________________________________________
-num_workers = 8
-tr_loader = DataLoader(tr_dataset, batch_size=config['BATCH_SIZE'], shuffle=True, num_workers=num_workers)
-val_loader = DataLoader(val_dataset, batch_size=config['BATCH_SIZE'], shuffle=False, num_workers=num_workers)
-tst_loader = DataLoader(tst_dataset, batch_size=config['BATCH_SIZE'], shuffle=False, num_workers=num_workers)
+# num_workers = 8
+# tr_loader = DataLoader(tr_dataset, batch_size=config['BATCH_SIZE'], shuffle=True, num_workers=num_workers)
+# val_loader = DataLoader(val_dataset, batch_size=config['BATCH_SIZE'], shuffle=False, num_workers=num_workers)
+# tst_loader = DataLoader(tst_dataset, batch_size=config['BATCH_SIZE'], shuffle=False, num_workers=num_workers)
 
-#___________________________________________________________________________________________________________________
-model = Classifier(model_obj)
+# #___________________________________________________________________________________________________________________
+# model = Classifier(model_obj)
 
-checkpoint_callback.dirpath = os.path.join(config['dir'], 'ckpts')
-checkpoint_callback.filename = config['ckpt_file_name']
-
-
-run_name = f"lr_{config['lr']} *** bs{config['BATCH_SIZE']} *** decay_{config['weight_decay']}"
-wandb_logger = WandbLogger(project=config['model_name'], name = run_name , log_model='all')
-csv_logger = CSVLogger(config['dir'], name=config['model_name']+'_logs')
+# checkpoint_callback.dirpath = os.path.join(config['dir'], 'ckpts')
+# checkpoint_callback.filename = config['ckpt_file_name']
 
 
-trainer = Trainer(callbacks=[early_stop_callback, checkpoint_callback, rich_progress_bar, rich_model_summary], 
-                  accelerator = 'gpu' ,max_epochs=200, logger=[wandb_logger,csv_logger])  
+# run_name = f"lr_{config['lr']} *** bs{config['BATCH_SIZE']} *** decay_{config['weight_decay']}"
+# wandb_logger = WandbLogger(project=config['model_name'], name = run_name , log_model='all')
+# csv_logger = CSVLogger(config['dir'], name=config['model_name']+'_logs')
+
+
+# trainer = Trainer(callbacks=[early_stop_callback, checkpoint_callback, rich_progress_bar, rich_model_summary], 
+#                   accelerator = 'cpu' ,max_epochs=0, logger=[wandb_logger,csv_logger])  
  
-trainer.fit(model, tr_loader, val_loader)
-trainer.test(model, tst_loader)
+# trainer.fit(model, tr_loader, val_loader)
+# trainer.test(model, tst_loader)
 
