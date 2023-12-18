2851ce799033b6e37bfaa7fac788c9b15b6b58ba
Sar2580P <sporwal1818@gmail.com>
2023-12-18 19:54:37 +0530

    created ensemble

--- models/ensemble/ensemble.py
+++ models/ensemble/ensemble.py
@@ -17,22 +17,26 @@ enet_obj = EffecientNet(load_config('models/rgb/enet/config.yaml'))
 gnet_obj = GoogleNet(load_config('models/rgb/google_net/config.yaml'))
 
 
-hsi_ckpt = os.path.join('models/hsi/xception/ckpts' , os.listdir('models/hsi/xception/ckpts')[-1])
+hsi_ckpt = os.path.join('models/hsi/dense_net/ckpts' , os.listdir('models/hsi/xception/ckpts')[-1])
 # resnet_ckpt = os.path.join('models/rgb/resnet/ckpts', os.listdir('models/rgb/resnet/ckpts')[-1])
 # enet_ckpt = os.path.join('models/rgb/enet/ckpts/' , os.listdir('models/rgb/enet/ckpts')[-1])
-gnet_ckpt = os.path.join('models/rgb/google_net/ckpts' , os.listdir('models/rgb/google_net/ckpts')[-1])
+rgb_ckpt = os.path.join('models/rgb/google_net/ckpts' , os.listdir('models/rgb/google_net/ckpts')[-1])
 
 hsi_classifier = Classifier.load_from_checkpoint(hsi_ckpt, model_obj=hsi_obj)
 # resnet_classifier = Classifier.load_from_checkpoint(resnet_ckpt, model_obj=resnet_obj)
 # enet_classifier = Classifier.load_from_checkpoint(enet_ckpt, model_obj=enet_obj)
-gnet_classifier = Classifier.load_from_checkpoint(gnet_ckpt, model_obj=gnet_obj)
+rgb_classifier = Classifier.load_from_checkpoint(rgb_ckpt, model_obj=gnet_obj)
 
 
 hsi_pretrained = nn.Sequential(*list(hsi_classifier.model.children())[:-1])
 # resnet_pretrained = nn.Sequential(*list(resnet_classifier.model.children())[:-1])
 # enet_pretrained = nn.Sequential(*list(enet_classifier.model.children())[:-1])
-gnet_pretrained = nn.Sequential(*list(gnet_classifier.model.children())[:-1])
+rgb_pretrained = nn.Sequential(*list(rgb_classifier.model.children())[:-1])
 
+print(rgb_pretrained)
+print('\n\n\n\n\n\n\n\n\n\n')
+print(hsi_pretrained)
+#_______________________________________________________________________________________________________________________
 for param in hsi_pretrained.parameters():
   param.requires_grad = False
   
@@ -42,7 +46,7 @@ for param in hsi_pretrained.parameters():
 # for param in enet_pretrained.parameters():
 #   param.requires_grad = False
 
-for param in gnet_pretrained.parameters():
+for param in rgb_pretrained.parameters():
   param.requires_grad = False
 
 #_______________________________________________________________________________________________________________________
@@ -53,8 +57,8 @@ class Ensemble(nn.Module):
 
     self.model = nn.Sequential(
                     # Dense(drop = 0.25 , in_size = 640, out_size = 1024, ), 
-                    Dense(drop = 0.15 , in_size = 640, out_size = 256, ), 
-                    Dense(drop = 0 , in_size = 256, out_size = self.config['num_classes']),
+                    FC(drop = 0.15 , in_size = 640, out_size = 256, ), 
+                    FC(drop = 0 , in_size = 256, out_size = self.config['num_classes']),
                     )
 
     self.layer_lr = self.model.parameters() 
@@ -65,8 +69,10 @@ class Ensemble(nn.Module):
     hsi_out = hsi_pretrained(x_hsi)
     # resnet_out = resnet_pretrained(x_rgb)
     # enet_out = enet_pretrained(x_rgb)
-    gnet_out =  gnet_pretrained(x_rgb)
-    out = self.model(torch.cat((hsi_out, gnet_out), dim=1))
+    rgb_out =  rgb_pretrained(x_rgb)
+    out = self.model(torch.cat((hsi_out, rgb_out), dim=1))
+    print('Radhe radhe' , out.shape)
+
     return out
 
 config = load_config('models/ensemble/config.yaml')
