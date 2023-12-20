25b18a26a947d29f540fe486f63abe7483629a61
Sar2580P <sporwal1818@gmail.com>
2023-12-20 18:24:47 +0530

    testing ensemble

--- models/ensemble/ensemble.py
+++ models/ensemble/ensemble.py
@@ -26,61 +26,65 @@ hsi_classifier = Classifier.load_from_checkpoint(hsi_ckpt, model_obj=denseNet_ob
 rgb_classifier = Classifier.load_from_checkpoint(rgb_ckpt, model_obj=gnet_obj)
 
 
-# hsi_pretrained = nn.Sequential(*list(hsi_classifier.model.children())[0][:-1])
-# rgb_pretrained = nn.Sequential(*list(rgb_classifier.model.children())[:-1])
-
+hsi_pretrained = nn.Sequential(*list(hsi_classifier.model.children()))
+rgb_pretrained = nn.Sequential(*list(rgb_classifier.model.children()))
+df = pd.read_csv('Data/96/df_tst.csv')
 #_______________________________________________________________________________________________________________________
-for param in hsi_classifier.parameters():
+for param in hsi_pretrained.parameters():
   param.requires_grad = False
 
-for param in rgb_classifier.parameters():
+for param in rgb_pretrained.parameters():
   param.requires_grad = False
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #_______________________________________________________________________________________________________________________
 def get_prediction(data_df ,classifier, classifier_name, input_type,  transforms_):
-  labels = data_df.iloc[: ,1]
+  labels = data_df.iloc[: ,2]
   df = pd.DataFrame(columns = [str(i) for i in range(96)])
   
-  for img_path in data_df.iloc[:,0]:
-      img = None
-      if input_type == 'hsi':
-        img = np.load(img_path)
+  for idx in range(len(data_df)):
+    img = None
+    if input_type == 'hsi':
+      img = np.load(data_df.iloc[idx , 1])
 
-      else :
-         img = cv2.imread(img_path)
+    else :
+      img = cv2.imread(data_df.iloc[idx , 0])
+    img = torch.unsqueeze(transforms_(img), 0)
+    output = torch.softmax(classifier(img.to(device)), 1)
+    output = output.detach().cpu().numpy()
+    df.loc[len(df)] = output[0]
 
-      img = transforms_(img)
-      print(img.shape)
-      output = torch.softmax(classifier(img))
-      output = output.detach().cpu().numpy()
-      df.loc[len(df)] = output
-
-  # df['labels'] = labels
+  df['labels'] = labels
    
-  # df.to_csv('models\ensemble\Classifier_Prediction_Data', classifier_name + '.csv' , index = False)
+  df.to_csv('models/ensemble/Classifier_Prediction_Data/'+ classifier_name + '.csv' , index = False)
   return df, labels
 
-p1, labels = get_prediction(df_tst ,hsi_classifier, 'denseNet_hsi_classifier', 'hsi',transforms.ToTensor())
-p2, _ = get_prediction(df_tst ,rgb_classifier, 'googleNet_rgb_classifier', 'rgb',transforms.ToTensor())
-parser = argparse.ArgumentParser()
-args = parser.parse_args()
+p1, labels = get_prediction(df_tst ,hsi_pretrained, 'denseNet_hsi_classifier', 'hsi',transforms.ToTensor())
+p2, _ = get_prediction(df_tst ,rgb_pretrained, 'googleNet_rgb_classifier', 'rgb',transforms.ToTensor())
 
-parser.add_argument('--topk', type=int, default = 2, help='Top-k number of classes')
+df_hsi = pd.read_csv('models/ensemble/Classifier_Prediction_Data/denseNet_hsi_classifier.csv')
+df_rgb = pd.read_csv('models/ensemble/Classifier_Prediction_Data/googleNet_rgb_classifier.csv')
 
-top = args.topk #top 'k' classes
+p1 , p2 , labels = df_hsi.iloc[: , :96] , df_rgb.iloc[: , :96] , df_hsi.iloc[: , 96]
+# parser = argparse.ArgumentParser()
+# args = parser.parse_args()
+
+# parser.add_argument('--topk', type=int, default = 2, help='Top-k number of classes')
+
+top = 3#top 'k' classes
 predictions = Gompertz(top, p1, p2)
 
 correct = np.where(predictions == labels)[0].shape[0]
 total = labels.shape[0]
 
 print("Accuracy = ",correct/total)
-classes = []
-for i in range(p1.shape[1]):
-    classes.append(str(i+1))
+# classes = []
+# for i in range(p1.shape[1]):
+#     classes.append(str(i+1))
     
-metrics(labels,predictions,classes)
+# metrics(labels,predictions,classes)
 
-plot_roc(labels,predictions)
+# plot_roc(labels,predictions)
 #_______________________________________________________________________________________________________________________
 
 
